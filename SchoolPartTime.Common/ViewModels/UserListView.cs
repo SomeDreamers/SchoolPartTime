075929db8f2d4d6ae86aabe304607ddd33ea07ea
XIANWEI HU <820693371@qq.com>
2017-04-08 11:17:16 +0800

    新增分页功能

---
+++ SchoolPartTime.Common/ViewModels/UserListView.cs
@@ -0,0 +1,16 @@
+﻿using SchoolPartTime.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPartTime.Common.ViewModels
+{
+    public class UserListView : PageViewModel
+    {
+        public List<User> Users { get; set; }
+        public UserListView(int page, int size, int total, List<User> users) : base(page, size, total)
+        {
+            Users = users;
+        }
+    }
+}
---
+++ SchoolPartTime.WebApp/Helpers/RoleHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using SchoolPartTime.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolPartTime.WebApp.Helpers
+{
+    public static class RoleHelper
+    {
+        /// <summary>
+        /// 获取角色下拉框
+        /// </summary>
+        /// <returns></returns>
+        public static List<SelectListItem> GetRoleItems(bool showNull = false)
+        {
+            List<SelectListItem> list = new List<SelectListItem>();
+            if (showNull)
+                list.Add(new SelectListItem { Text = "请选择", Value = "0"});
+            foreach (RoleType item in Enum.GetValues(typeof(RoleType)))
+            {
+                list.Add(new SelectListItem { Text = item.GetTextVaule(), Value = item.GetValue().ToString() });
+            }
+            return list;
+        }
+    }
+}
